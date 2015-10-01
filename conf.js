c81466abf881ffd7b6042bf935778a3e1bfda8dd
mjvdende <maartenvdende@gmail.com>
2015-10-01 21:52:16 +0200

    use jasmine 2

--- conf.js
+++ conf.js
@@ -1,9 +1,14 @@
 exports.config = {
   directConnect: true,
   specs: ['*-spec.js'],
+  framework: 'jasmine2',
   onPrepare: function() {
     require('jasmine-reporters');
     jasmine.getEnv().addReporter(
-      new jasmine.JUnitXmlReporter('report', true, true));
+      new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: 'report',
+        filePrefix: 'xmloutput'
+      }));
   }
 };
