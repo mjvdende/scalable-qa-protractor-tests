6354770abd0639b379ebb5ed180abfe6fccdf855
mjvdende <maartenvdende@gmail.com>
2015-10-01 21:13:29 +0200

    only chrome

--- conf.js
+++ conf.js
@@ -1,9 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://172.17.8.102:4448/wd/hub',
-  specs: ['*-spec.js'],
-  multiCapabilities: [{
-    'browserName': 'firefox'
-	}, {
-    'browserName': 'chrome'
-	}]
+  specs: ['*-spec.js']
 };
