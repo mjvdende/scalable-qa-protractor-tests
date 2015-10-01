338dc44191e8552798dace16e7f8eb8bfc92721d
mjvdende <maartenvdende@gmail.com>
2015-10-01 15:31:25 +0200

    run local

--- conf.js
+++ conf.js
@@ -1,10 +1,4 @@
 exports.config = {
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
   directConnect: true,
-  specs: ['*-spec.js'],
-  multiCapabilities: [{
-    'browserName': 'firefox'
-	}, {
-    'browserName': 'chrome'
-	}]
+  specs: ['*-spec.js']
 };
