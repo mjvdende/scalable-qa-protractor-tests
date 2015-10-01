e4a314257e01b8ba4b178c6b3407bcd814bf2fc1
mjvdende <maartenvdende@gmail.com>
2015-10-01 21:24:41 +0200

    reformat

--- conf.js
+++ conf.js
@@ -1,10 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://172.17.8.102:4448/wd/hub',
   specs: ['*-spec.js']
-
-	onPrepare: function() {
+  onPrepare: function() {
     require('jasmine-reporters');
     jasmine.getEnv().addReporter(
       new jasmine.JUnitXmlReporter(outputxmldir', true, true));
-	}
+  }
 };
