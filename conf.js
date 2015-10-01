40d345a17e907bec204d5eae2553c03b747f5c3b
mjvdende <maartenvdende@gmail.com>
2015-10-01 22:02:56 +0200

    typo

--- conf.js
+++ conf.js
@@ -1,14 +1,8 @@
+require('jasmine-reporters');
 exports.config = {
   directConnect: true,
   specs: ['*-spec.js'],
   framework: 'jasmine2',
   onPrepare: function() {
-    var jasmineReporters = require('jasmine-reporters');
-    jasmine.getEnv().addReporter(
-      new jasmineReporters.JUnitXmlReporter({
-        consolidateAll: true,
-        savePath: 'report',
-        filePrefix: 'xmloutput'
-      }));
-  }
+  jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('', true, true));
 };
