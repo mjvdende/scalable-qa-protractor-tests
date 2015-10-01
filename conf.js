c52794c24f50e5eab1ab341cd8e457a9f0d42cbe
mjvdende <maartenvdende@gmail.com>
2015-10-01 15:23:32 +0200

    add test

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   directConnect: true,
-  specs: ['todo-spec.js'],
+  specs: ['*-spec.js'],
   multiCapabilities: [{
     'browserName': 'firefox'
 	}, {
