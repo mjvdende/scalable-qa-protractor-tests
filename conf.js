0317afc3f5cace4803364743ae226e2bd4348d9e
mjvdende <maartenvdende@gmail.com>
2015-10-01 21:20:34 +0200

    add reports

--- conf.js
+++ conf.js
@@ -1,4 +1,10 @@
 exports.config = {
   seleniumAddress: 'http://172.17.8.102:4448/wd/hub',
   specs: ['*-spec.js']
+
+	onPrepare: function() {
+    require('jasmine-reporters');
+    jasmine.getEnv().addReporter(
+      new jasmine.JUnitXmlReporter(outputxmldir', true, true));
+	}
 };
